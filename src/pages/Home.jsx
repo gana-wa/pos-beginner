acbe9bf2a508111b37445ef31ef2258b519e0fd9
ganawa <ghana.afani@gmail.com>
2020-08-10 04:14:49 +0700

    commit 1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,11 +18,25 @@ class Home extends Component {
       menus: [],
       cart: [
          {
-            product_name: "Esspresso",
+            product_name: "Cappucino",
             image: "https://week3-pos.netlify.app/assets/firdaus-roslan-pN769u0KHNA-unsplash.png",
-            price: 5000
+            price: 5000,
+            quantity: 2
+         },
+         {
+            product_name: "Esspresso",
+            image: "https://week3-pos.netlify.app/assets/jeremy-ricketts-6ZnhM-xBpos-unsplash.png",
+            price: 10000,
+            quantity: 3
+         },
+         {
+            product_name: "Black Forest",
+            image: "https://week3-pos.netlify.app/assets/blackforest.png",
+            price: 30000,
+            quantity: 1
          }
-      ]
+      ],
+      totalPrice: 0
    };
 
    componentDidMount = () => {
@@ -36,37 +50,76 @@ class Home extends Component {
          .catch(err => console.log(err))
    }
 
-   render() {
-      const getCart = () => {
-         if (this.state.cart.length) {
-            return (
-               this.state.cart.map((item) => {
-                  return (
-                     <div className="row">
-                        <div className="col">
-                           <img src={item.image} className="w-50" alt="" />
-                           <h5>{item.product_name}</h5>
-                           <div className="btn-group mt-auto" role="group" aria-label="Basic example">
-                              <button type="button" className="btn btn-success ">-</button>
-                              <button type="button" className="btn btn-outline-success">1</button>
-                              <button type="button" className="btn btn-success ">+</button>
-                           </div>
-                           <h6>Rp {item.price}</h6>
+   showCart = () => {
+      if (this.state.cart.length) {
+         return <>
+            {this.state.cart.map((item) => {
+               return (
+                  <div className="row my-5">
+                     <div className="col">
+                        <img src={item.image} className="w-75 rounded-lg" alt="" />
+                     </div>
+                     <div className="col">
+                        <h5>{item.product_name}</h5>
+                        <div className="btn-group mt-auto" role="group" aria-label="Basic example">
+                           <button type="button" className="btn btn-success">-</button>
+                           <h5 className="m-2">{item.quantity}</h5>
+                           <button type="button" className="btn btn-success" onClick={
+                              this.handleIncrementQuantity
+                           } >+</button>
                         </div>
                      </div>
-                  )
-               })
-            )
-         } else {
-            return (
-               <>
-                  <img src={logoCart} className="w-50" alt="" />
-                  <h5>Your cart is empty</h5>
-                  <p className="text-secondary">Please add some items from the menu</p>
-               </>
-            )
-         }
+                     <div className="col d-flex">
+                        <h6 className="mt-auto ml-auto">Rp {item.price * item.quantity}</h6>
+                     </div>
+                  </div>
+               )
+            })}
+            <div className="row mx-2">
+               <div className="col d-flex">
+                  <h4 className="mr-auto font-weight-bold">Total :</h4>
+               </div>
+
+               <div className="col d-flex">
+                  <h4 className="ml-auto font-weight-bold">Rp {this.state.totalPrice}*</h4>
+               </div>
+            </div>
+            <div className="row mx-2">
+               <div className="col d-flex">
+                  <p className="mr-auto">*Belum termasuk ppn</p>
+               </div>
+            </div>
+            <div className="row mx-2 my-2">
+               <div className="col">
+                  <button
+                     type="button"
+                     className="btn btn-info btn-block btn-lg">
+                     Checkout
+                  </button>
+               </div>
+            </div>
+            <div className="row mx-2 my-2">
+               <div className="col">
+                  <button
+                     type="button"
+                     className="btn btn-danger btn-block btn-lg">
+                     Cancel
+                  </button>
+               </div>
+            </div>
+         </>
+      } else {
+         return (
+            <>
+               <img src={logoCart} className="w-50" alt="" />
+               <h5>Your cart is empty</h5>
+               <p className="text-secondary">Please add some items from the menu</p>
+            </>
+         )
       }
+   }
+
+   render() {
       return (
          <>
             <div className="container-fluid">
@@ -105,7 +158,7 @@ class Home extends Component {
                      </div>
                      <div className="row h-100"> {/* masalah disini (h-100) */}
                         <div className="col bg-white text-center border-top">
-                           {getCart()}
+                           {this.showCart()}
                         </div>
                      </div>
                   </div>
