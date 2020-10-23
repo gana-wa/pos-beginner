133fc0316d5839b530fdffb64926e7b98a6ef27c
ganawa <ghana.afani@gmail.com>
2020-10-23 18:42:37 +0700

    [update] cheklist img

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { addToCart } from '../redux/actions/menu';
 
-// import './styles/Menu.css';
+import './styles/Menu.css';
 import tick from '../assets/img/tick.png';
 
 const Menu = (props) => {
@@ -11,23 +11,28 @@ const Menu = (props) => {
          <div className="row py-3">
             {props.menu.menus.map((item) => {
                return (
-                  <div className="col-6 col-sm-4 item-menu" key={item.product_id}>
-                     {
-                        item.selected ? (
-                           <img src={tick} alt="" />
-                        ) : ("")
-                     }
-                     <img src={item.image}
-                        className="card-img-top"
-                        onClick={() => props.addToCart(
-                           item.product_id,
-                           item.product_name,
-                           item.price,
-                           item.image
-                        )}
-                        alt={`gambar ${item.product_name}`} />
-                     <h5>{item.product_name}</h5>
-                     <h5 className="font-weight-bold">Rp {item.price.toLocaleString()}</h5>
+                  <div className="col-6 col-sm-4" key={item.product_id}>
+                     <div className="container-card-menu" onClick={() => props.addToCart(
+                        item.product_id,
+                        item.product_name,
+                        item.price,
+                        item.image
+                     )}>
+                        <div className="container-img-menu">
+                           {
+                              item.selected ? (
+                                 <div className="checked">
+                                    <img src={tick} alt="" />
+                                 </div>
+                              ) : ("")
+                           }
+                           <img src={item.image}
+                              className="menu-img"
+                              alt={`gambar ${item.product_name}`} />
+                        </div>
+                        <h5>{item.product_name}</h5>
+                        <h5 className="font-weight-bold">Rp {item.price.toLocaleString('id-ID')}</h5>
+                     </div>
                   </div>
                )
             })
